8f9a60c9afa1b195bf65c8b4a5967433888217f1
utkarsh chaturvedi <utkarshchaturvedi890@gmail.com>
2024-04-20 20:28:02 +0530

    userInfo, logout, middleware, bcrypt password

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,11 @@
 import  Dotenv  from "dotenv";
-
+import app from "./app.js";
+/* importing app.js because i have import express in the app.js so , we are creating a server using express ,bcz expre is inside 'const app = express()'  */
 Dotenv.config({
     path: './env'
 });
 
 const PORT  = process.env.PORT ;
-
-import app from "./app.js";
 
 app.listen(PORT , () => {
     console.log(`server is listining at http://localhost:${PORT}`)
